c7c7b192e7ff105047e60618df333ba30d91a7a3
Heli Eronen <herojonen@gmail.com>
2022-03-18 15:53:53 +0200

    Project created

---
+++ cipher-decipher/src/main/java/cipher/decipher/Main.java
@@ -0,0 +1,14 @@
+
+package cipher.decipher;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!");
+    }
+    
+}
---
+++ dokumentaatio/Viikkoraportti_1.md
@@ -0,0 +1,11 @@
+# Viikkoraportti 1
+
+### Mitä olen tehnyt tällä viikolla?
+
+### Miten ohjelma on edistynyt?
+
+### Mitä opin tällä viikolla/tänään?
+
+### Mikä jäi epäselväksi tai tuottanut vaikeuksia?
+
+### Mitä teen seuraavaksi?
